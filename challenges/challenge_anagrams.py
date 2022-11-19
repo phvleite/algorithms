59e97e7f8f1832f201150e481fa8aa275eb004fc
phvleite <phvleite@gmail.com>
2022-11-19 20:02:29 -0300

    refactor: mundança do nome da variável, de NUMBERS para LIST_STRINGS, para ficar mais verboso

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -36,34 +36,34 @@ def convert_list_to_strings(list):
     return "".join(list)
 
 
-def merge_sort(numbers, start=0, end=None):
+def merge_sort(list_strings, start=0, end=None):
     if end is None:
-        end = len(numbers)
+        end = len(list_strings)
     if (end - start) > 1:
         mid = (start + end) // 2
-        merge_sort(numbers, start, mid)
-        merge_sort(numbers, mid, end)
-        merge(numbers, start, mid, end)
+        merge_sort(list_strings, start, mid)
+        merge_sort(list_strings, mid, end)
+        merge(list_strings, start, mid, end)
 
 
-def merge(numbers, start, mid, end):
-    left = numbers[start:mid]
-    right = numbers[mid:end]
+def merge(list_strings, start, mid, end):
+    left = list_strings[start:mid]
+    right = list_strings[mid:end]
 
     left_index, right_index = 0, 0
 
     for general_index in range(start, end):
         if left_index >= len(left):
-            numbers[general_index] = right[right_index]
+            list_strings[general_index] = right[right_index]
             right_index = right_index + 1
         elif right_index >= len(right):
-            numbers[general_index] = left[left_index]
+            list_strings[general_index] = left[left_index]
             left_index = left_index + 1
         elif left[left_index] < right[right_index]:
-            numbers[general_index] = left[left_index]
+            list_strings[general_index] = left[left_index]
             left_index = left_index + 1
         else:
-            numbers[general_index] = right[right_index]
+            list_strings[general_index] = right[right_index]
             right_index = right_index + 1
 
 
