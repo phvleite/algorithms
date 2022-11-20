01d5d1f995079cb77e73041f7ec9a0e801d7b0e8
phvleite <phvleite@gmail.com>
2022-11-20 07:39:46 -0300

    fix: em busca da correção do bug deste teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -7,27 +7,37 @@ MOCK_MESSAGES_KEYS = [
     ("ABCDEF", -1, "FEDCBA"),
     ("ABCDEF", 3, "CBA_FED"),
     ("ABCDEF", 4, "FE_DCBA"),
+    ("CHEWBACCA", 20, "CHEWBACCA"),
 ]
 
 
 def test_encrypt_message():
-    with pytest.raises(TypeError, match="tipo inválido para key"):
+    with pytest.raises(TypeError) as err:
         encrypt_message(MOCK_MESSAGES_KEYS[0][0], MOCK_MESSAGES_KEYS[0][1])
+        raise TypeError("tipo inválido para key")
+    assert err.type is TypeError
 
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(MOCK_MESSAGES_KEYS[1][0], MOCK_MESSAGES_KEYS[1][1])
+        raise TypeError("tipo inválido para message")
+    assert err.type is TypeError
 
-    assert (
-        encrypt_message(MOCK_MESSAGES_KEYS[2][0], MOCK_MESSAGES_KEYS[2][1])
-        == MOCK_MESSAGES_KEYS[2][2]
+    result = encrypt_message(
+        MOCK_MESSAGES_KEYS[2][0], MOCK_MESSAGES_KEYS[2][1]
     )
+    assert result == MOCK_MESSAGES_KEYS[2][2]
 
-    assert (
-        encrypt_message(MOCK_MESSAGES_KEYS[3][0], MOCK_MESSAGES_KEYS[3][1])
-        == MOCK_MESSAGES_KEYS[3][2]
+    result = encrypt_message(
+        MOCK_MESSAGES_KEYS[3][0], MOCK_MESSAGES_KEYS[3][1]
     )
+    assert result == MOCK_MESSAGES_KEYS[3][2]
 
-    assert (
-        encrypt_message(MOCK_MESSAGES_KEYS[4][0], MOCK_MESSAGES_KEYS[4][1])
-        == MOCK_MESSAGES_KEYS[4][2]
+    result = encrypt_message(
+        MOCK_MESSAGES_KEYS[4][0], MOCK_MESSAGES_KEYS[4][1]
     )
+    assert result == MOCK_MESSAGES_KEYS[4][2]
+
+    result = encrypt_message(
+        MOCK_MESSAGES_KEYS[5][0], MOCK_MESSAGES_KEYS[5][1]
+    )
+    assert result != MOCK_MESSAGES_KEYS[5][2]
