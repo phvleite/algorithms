ff2a12d438815673ceb1220f157d821e3d2e4425
phvleite <phvleite@gmail.com>
2022-11-21 17:03:40 -0300

    fix: corrigido o problema com o tempo de execução da função challenge_palindromes_iterative.py

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,28 +1,59 @@
 def is_palindrome_iterative(word):
     pass
-    # if len(word) == 0 or word[0] != word[-1]:
-    #     return False
-    # if len(word) == 1:
-    #     return True
+    if len(word) == 0 or word[0] != word[-1]:
+        return False
+    if len(word) == 1:
+        return True
 
-    # word = word.lower()
-    # is_palindrome = False
-    # ind = 0
-    # ind_max = len(word)
-    # for ind in range(len(word)):
+    word = word.lower()
+    ind = 0
+    ind_max = len(word) - 1
+    for ind in range(len(word)):
+        if 2 >= ind_max - ind >= 1 and word[ind] == word[ind_max]:
+            return True
+        ind_max -= 1
 
-    #     if 3 >= len(word) > 1 and word[0] == word[-1]:
-    #         is_palindrome = True
-    #     word = word[1:-1]
-
-    # # while len(word) >= 1:
-    # #     if 3 >= len(word) > 1 and word[0] == word[-1]:
-    # #         is_palindrome = True
-    # #     word = word[1:-1]
-
-    # return is_palindrome
+    return False
 
 
 if __name__ == "__main__":
-    word = ""
-    print(is_palindrome_iterative(word))
+    WORDS = [
+        "ala",
+        "ama",
+        "anã",
+        "anilina",
+        "arara",
+        "ata",
+        "ele",
+        "esse",
+        "mamam",
+        "matam",
+        "metem",
+        "mirim",
+        "oco",
+        "omissíssimo",
+        "osso",
+        "ovo",
+        "radar",
+        "raiar",
+        "ralar",
+        "reger",
+        "reler",
+        "reter",
+        "rever",
+        "reviver",
+        "rir",
+        "rodador",
+        "sacas",
+        "saias",
+        "salas",
+        "socos",
+        "sós",
+        "berinjela",
+        "",
+        "j",
+    ]
+    for word in WORDS:
+        print(
+            f"a palvra {word} é um palindromo? {is_palindrome_iterative(word)}"
+        )
