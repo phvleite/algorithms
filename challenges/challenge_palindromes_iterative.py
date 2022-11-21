910023c65929b4356e6f55d495ac9812a858378b
phvleite <phvleite@gmail.com>
2022-11-21 11:17:13 -0300

    feat: implementada a função is_palindrome_iterative() em challenge_palindromes_iterative.py

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,19 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or (word[0] != word[-1]):
+        return False
+    elif len(word) == 1:
+        return True
+
+    word = word.lower()
+    is_palindrome = False
+    while len(word) > 0:
+        if 3 >= len(word) > 1 and word[0] == word[-1]:
+            is_palindrome = True
+        word = word[1:-1]
+
+    return is_palindrome
+
+
+if __name__ == "__main__":
+    word = "GulauG"
+    print(is_palindrome_iterative(word))
