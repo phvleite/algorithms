5334b7c778534c9734385f241c34ed9992393ba5
phvleite <phvleite@gmail.com>
2022-11-21 19:29:02 -0300

    feat: implementada a função find_duplicate() em challenge_find_the_duplicate.py

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,27 +1,13 @@
 def find_duplicate(nums):
-    pass
-    # if not verify_list(nums):
-    #     return False
+    if not verify_list(nums):
+        return False
 
-    # nums.sort()
+    nums.sort()
 
-    # for ind, num in enumerate(nums):
-    #     for aux in nums[i + 1:]:
-    #         if nums[i] == aux:
-    #             return aux
-
-    # return False
-
-
-# def find_duplicate_aux(nums, number, next, size):
-#     if nums[number] == nums[next]:
-#         return nums[number]
-#     elif number + 1 == size - 1:
-#         return False
-#     elif next + 1 == size:
-#         return find_duplicate_aux(nums, number + 1, number + 2, size)
-#     else:
-#         return find_duplicate_aux(nums, number, next + 1, size)
+    for ind, num in enumerate(nums):
+        if ind <= len(nums) - 2 and num == nums[ind + 1]:
+            return num
+    return False
 
 
 def verify_list(nums):
@@ -42,5 +28,6 @@ def verify_string_and_not_negative_in_list(nums):
 
 
 if __name__ == "__main__":
-    list_nums = [52, 99, 58, 20, 6, 999, 5, 41, 38, 9]
+    # list_nums = [52, 99, 53, 20, 6, 999, 5, 41, 38, 9]
+    list_nums = [1, 3, 4, 2, 22]
     print(find_duplicate(list_nums))
